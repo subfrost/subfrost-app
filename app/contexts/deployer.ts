3c970bfcd9e981d6b3be8b81f333f66800a2549f
sprimage <timilehin1999@gmail.com>
2025-01-14 20:36:04 -0600

    update

--- app/contexts/deployer.ts
+++ app/contexts/deployer.ts
@@ -1,8 +1,8 @@
 import { Wallet } from './wallet'
-// import { deployCommit, deployReveal } from '@oyl/sdk/lib/alkanes'
+import { deployCommit, deployReveal } from '@oyl/sdk/lib/alkanes'
 import { accountUtxos } from '@oyl/sdk/lib/utxo'
 import { Provider } from '@oyl/sdk/lib/provider'
-
+import { timeout } from './sandshrew-provider'
 const DEFAULT_RESERVE_NUMBER = '0x7'
 
 const POLL_INTERVAL = 3000;
@@ -17,7 +17,7 @@ async function waitForIndex(provider: Provider): Promise<void> {
         console.log("indexer caught up");
         break;
       } else {
-        //await timeout(POLL_INTERVAL);
+        await timeout(POLL_INTERVAL);
         console.log("retry poll");
       }
     }
@@ -38,61 +38,61 @@ export async function contractDeployer(options: any) {
     const { accountSpendableTotalUtxos, accountSpendableTotalBalance } =
       await accountUtxos({ account: wallet.account, provider: wallet.provider })
 
-    //   const { txId: commitTxId, script } = await deployCommit({
-    //     payload,
-    //     gatheredUtxos: {
-    //       utxos: accountSpendableTotalUtxos,
-    //       totalAmount: accountSpendableTotalBalance,
-    //     },
-    //     feeRate: wallet.feeRate,
-    //     account: wallet.account,
-    //     signer: wallet.signer,
-    //     provider: wallet.provider,
-    //   })
+      const { txId: commitTxId, script } = await deployCommit({
+        payload,
+        gatheredUtxos: {
+          utxos: accountSpendableTotalUtxos,
+          totalAmount: accountSpendableTotalBalance,
+        },
+        feeRate: wallet.feeRate,
+        account: wallet.account,
+        signer: wallet.signer,
+        provider: wallet.provider,
+      })
   
-    //   console.log('Commit txid: ', commitTxId);
+      console.log('Commit txid: ', commitTxId);
   
   
-    //   const mempool = await wallet.provider.sandshrew._call('getRawMemPool', [true])
-    //   const mempoolTxs = Object.keys(mempool)
-    //   console.log('mempool transactions: ', mempoolTxs)
+      const mempool = await wallet.provider.sandshrew._call('getRawMemPool', [true])
+      const mempoolTxs = Object.keys(mempool)
+      console.log('mempool transactions: ', mempoolTxs)
   
-    //   const blockHash = await wallet.provider.sandshrew._call('generateBlock', [
-    //     wallet.account.nativeSegwit.address,
-    //     mempoolTxs
-    //   ])
+      const blockHash = await wallet.provider.sandshrew._call('generateBlock', [
+        wallet.account.nativeSegwit.address,
+        mempoolTxs
+      ])
   
-    //   console.log('Block hash: ', blockHash)
+      console.log('Block hash: ', blockHash)
   
-    //   await waitForIndex(wallet.provider)
+      await waitForIndex(wallet.provider)
   
-    //   const { txId: revealTxId } = await deployReveal({
-    //     createReserveNumber: options.reserveNumber || DEFAULT_RESERVE_NUMBER,
-    //     commitTxId: commitTxId,
-    //     script: script,
-    //     account: wallet.account,
-    //     provider: wallet.provider,
-    //     feeRate: wallet.feeRate,
-    //     signer: wallet.signer,
-    //   })
+      const { txId: revealTxId } = await deployReveal({
+        createReserveNumber: options.reserveNumber || DEFAULT_RESERVE_NUMBER,
+        commitTxId: commitTxId,
+        script: script,
+        account: wallet.account,
+        provider: wallet.provider,
+        feeRate: wallet.feeRate,
+        signer: wallet.signer,
+      })
   
-    //   console.log('Reveal txid: ', revealTxId)
+      console.log('Reveal txid: ', revealTxId)
   
-    //   const mempool2 = await wallet.provider.sandshrew._call('getRawMemPool', [true])
-    //   const mempoolTxs2 = Object.keys(mempool2)
-    //   console.log('mempool transactions: ', mempoolTxs2)
-    //   const blockHash2 = await wallet.provider.sandshrew._call('generateBlock', [
-    //     wallet.account.nativeSegwit.address,
-    //     mempoolTxs2
-    //   ])
-    //   console.log('Block hash: ', blockHash2)
-    //   await waitForIndex(wallet.provider)
+      const mempool2 = await wallet.provider.sandshrew._call('getRawMemPool', [true])
+      const mempoolTxs2 = Object.keys(mempool2)
+      console.log('mempool transactions: ', mempoolTxs2)
+      const blockHash2 = await wallet.provider.sandshrew._call('generateBlock', [
+        wallet.account.nativeSegwit.address,
+        mempoolTxs2
+      ])
+      console.log('Block hash: ', blockHash2)
+      await waitForIndex(wallet.provider)
   
-    //   const contractTrace = await wallet.provider.alkanes.trace({
-    //     txid: revealTxId,
-    //     vout: 3
-    //   });
+      const contractTrace = await wallet.provider.alkanes.trace({
+        txid: revealTxId,
+        vout: 3
+      });
   
-    //   console.log('Contract trace: ', contractTrace)
+      console.log('Contract trace: ', contractTrace)
 
 }
--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 const {webpack} = require("next/dist/compiled/webpack/webpack");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  future: {
+    webpack5: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Don't attempt to load these modules on the client side
+  webpack: (config) => {
       config.resolve.fallback = {
-        fs: false,
         net: false,
         tls: false,
         crypto: false,
+        fs: false,
         zlib: false,
         path: false,
         stream: false,
-        'cbor-x/decode': false,
         http: false,
         https: false,
         'child_process': false,
-        'graceful-fs': false,
         'module': false,
+        'fs-extra': false,
+        'graceful-fs': false
       }
-    }
 
   
     config.plugins.push(new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
